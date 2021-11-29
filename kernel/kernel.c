98707086ac11faf2ed5ed9a1c7dc3b484478b509
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-11-29 11:05:47 +0200

    .clang-format: Add a simple clang format file.
    While at it, properly cast the UART defines and format the files
    according to the .clang-format file.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,14 +3,15 @@
 
 uint8_t *uart = (uint8_t *) 0x10000000;
 
-#define UART                0x10000000
-#define UART_THR            (uint8_t *)(UART + 0x00)
-#define UART_LSR            (uint8_t *)(UART + 0x05)
-#define UART_LSR_EMPTY_MASK 0x40
+#define UART                (uint8_t *) 0x10000000
+#define UART_THR            (uint8_t *) (UART + 0x00)
+#define UART_LSR            (uint8_t *) (UART + 0x05)
+#define UART_LSR_EMPTY_MASK (uint8_t) 0x40
 
 int putchar(char ch) {
-	while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0);
-	return *UART_THR = ch;
+    while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0)
+        ;
+    return *UART_THR = ch;
 }
 
 int puts(const char *str) {
@@ -25,5 +26,6 @@ int puts(const char *str) {
 
 void kmain(void) {
     puts("Oracular Spectacular\n");
-    while (1);
+    while (1)
+        ;
 }
