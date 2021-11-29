6ff53cca45996b507ca9a6f1bc32dd6cc8b1fa80
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-11-29 10:05:56 +0200

    Toolchain: Add a script to download and install RISCV gcc-11.1.0

---
+++ toolchain/gcc/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+ORACULAR_TOP_LEVEL=$(git rev-parse --show-toplevel)
+ORACULAR_GCC_DIR=$ORACULAR_TOP_LEVEL/toolchain/gcc
+
+EXEC_DIR=$(pwd)
+
+if [ "$EXEC_DIR" != "$ORACULAR_GCC_DIR" ]; then
+    echo 'script must be run from toolchain/gcc dir.'
+    exit
+fi
+
+if [ "$1" = "clean" ]; then
+    rm -rf gcc-build
+    exit
+fi
+
+which riscv64-linux-gcc > /dev/null && exit
+
+if [ "$PREFIX" = "" ]; then
+    echo "export PREFIX=\"\$HOME/opt/cross\"" >> ~/.bashrc
+    echo "export TARGET=\"riscv64-elf\"" >> ~/.bashrc
+    echo "export PATH=\"\$PREFIX/bin:\$PATH\"" >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+if ! [ -d gcc-src ]; then
+    mkdir -v gcc-src
+    cd gcc-src
+
+    echo 'Fetching gcc version 11.1.0'
+    curl -O http://mirror.veriteknik.net.tr/gnu/gcc/gcc-11.1.0/gcc-11.1.0.tar.gz
+
+    echo 'Extracting gcc'
+    tar xf gcc-11.1.0.tar.gz
+
+    cd ../
+fi
+
+which $TARGET-as || { echo 'Could not find $(TARGET)-as; exitting'; exit 1; }
+
+mkdir -p -v gcc-build
+cd gcc-build
+
+../gcc-src/gcc-11.1.0/configure \
+    --target=$TARGET            \
+    --prefix=$PREFIX            \
+    --disable-nls               \
+    --without-headers           \
+    --enable-languages=c,c++
+
+make all-gcc -j$(nproc)
+make all-target-libgcc -j$(nproc)
+make install-gcc -j$(nproc)
+make install-target-libgcc -j$(nproc)
