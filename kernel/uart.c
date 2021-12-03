f4bb8d94d91a83295d75419df19e48e3239409e3
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-12-03 13:43:27 +0200

    UART: Enable FIFO and fix BAUD rate.
    We set the BAUD rate according to the datasheet and enable FIFO.

--- kernel/uart.c
+++ kernel/uart.c
@@ -6,20 +6,33 @@ void uart0_init() {
     /* Mask all UART interrupts. */
     mmio_write_byte(UART0_BASE, UART_IER, 0x00);
 
-    /* Enable Baud rate configuration */
+    /* Enable FIFO*/
+    mmio_write_byte(UART0_BASE, UART_FCR, (1 << 0));
+
+    /* Enable Baud rate configuration by setting the DLAB bit. */
     mmio_write_byte(UART0_BASE, UART_LCR, (1 << 7));
 
-    /* Set Baud rate LSB: 115.2K, the fastest possible rate. */
-    mmio_write_byte(UART0_BASE, UART_BAUD_LSB, 0x01);
+    /*
+     * The formula for calculating the Baud rate: BR = f_clk / (16 * divisor)
+     * Therefore, divisor = f_clk / (16 * BR). If we want to acheive a BAUD rate
+     * of 9600 bps, then the divisor must be 147.97526.
+     */
+    uint16_t divisor = 148;
+
+    /* Set Baud rate LSB */
+    mmio_write_byte(UART0_BASE, UART_BAUD_LSB, (divisor & 0xFF));
 
     /* Set Baud rate MSB */
-    mmio_write_byte(UART0_BASE, UART_BAUD_MSB, 0x00);
+    mmio_write_byte(UART0_BASE, UART_BAUD_MSB, (divisor >> 8));
 
     /* Leave Baud configuration mode. */
     mmio_write_byte(UART0_BASE, UART_LCR, 0x00);
 
     /* Set the word length to 8 bits with no parity */
     mmio_write_byte(UART0_BASE, UART_LCR, 0x03);
+
+    /* Enable receive interrupts */
+    mmio_write_byte(UART0_BASE, UART_IER, (1 << 0));
 }
 
 void uart0_putc(char c) {
