a9d6c724242cf8c5f9cc04f9b27ffe40c5d6f329
Mahmoud Abumandour <ma.mandourr@gmail.com>
2022-05-27 01:34:30 +0200

    kernel: Refactor prompt, make it more visible that it's endless.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,6 +5,22 @@
 #include <kernel/vm.h>
 #include <stdint.h>
 
+__attribute__((noreturn)) void prompt()
+{
+    while (1) {
+        char c = read_char();
+        if (c == 0x7f) {
+            putchar(8);
+            putchar(' ');
+            putchar(8);
+        } else if (c == 10 || c == 13) {
+            putchar('\n');
+        } else {
+            putchar(c);
+        }
+    }
+}
+
 void kmain(void)
 {
     uart0_init();
@@ -25,16 +41,5 @@ void kmain(void)
     set_sstatus(get_sstatus() | (1 << 1));
     kprintf("done\n");
 
-    while (1) {
-        char c = read_char();
-        if (c == 0x7f) {
-            putchar(8);
-            putchar(' ');
-            putchar(8);
-        } else if (c == 10 || c == 13) {
-            putchar('\n');
-        } else {
-            putchar(c);
-        }
-    }
+    prompt();
 }
