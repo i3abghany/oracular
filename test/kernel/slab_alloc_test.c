5b63d6486edbcadba986fa0d347654f6dc215999
Mahmoud Abumandour <ma.mandourr@gmail.com>
2022-06-04 15:17:11 +0200

    test/slab: More tests for slab_free().

--- test/kernel/slab_alloc_test.c
+++ test/kernel/slab_alloc_test.c
@@ -24,19 +24,27 @@ void test1()
         ASSERT_NOT_NULL(sss);
         uint64_t expected_addr =
             (uint64_t) s1_slab + PAGE_SIZE - sizeof(struct s1) * i + diff;
-        // kprintf("%p, %p\n", sss, expected_addr);
         ASSERT_EQUAL(expected_addr, (uint64_t) sss);
     }
-
 }
 
 void test2()
 {
     struct slab_t *s1_slab = slab_init("s1", sizeof(struct s1));
     struct s1 *sss1 = slab_alloc(s1_slab);
-    slab_free(s1_slab, (void *)sss1);
+    slab_free(s1_slab, (void *) sss1);
     struct s1 *sss2 = slab_alloc(s1_slab);
     ASSERT_EQUAL(sss1, sss2);
+
+    struct s1 *sss3 = slab_alloc(s1_slab);
+    struct s1 *sss4 = slab_alloc(s1_slab);
+    slab_free(s1_slab, sss3);
+    slab_free(s1_slab, sss4);
+    struct s1 *sss5 = slab_alloc(s1_slab);
+    struct s1 *sss6 = slab_alloc(s1_slab);
+
+    ASSERT_EQUAL(sss3, sss6);
+    ASSERT_EQUAL(sss4, sss5);
 }
 
 void slab_alloc_tests()
