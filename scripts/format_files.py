441e7794216347f7dd1da8afba229fe2d1de2be6
Mahmoud Abumandour <ma.mandourr@gmail.com>
2022-06-05 14:54:08 +0200

    scripts: Add an automatic formating script.

---
+++ scripts/format_files.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__) + "/..")
+    files = subprocess.run(['git', 'ls-files', '--', '*.c', '*.h'], check=True, capture_output=True).stdout.decode().strip('\n').split('\n')
+
+    argv = ['clang-format']
+    argv += files
+    argv.append('-i')
+    print(argv)
+    subprocess.run(argv)
+
+
